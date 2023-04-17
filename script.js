b2b1f2200927764bb70741a9a3f468a647bea1c0
rodjus19@gmail.com <gusrod22>
2023-04-17 00:03:18 -0400

    created image transition with javascript

---
+++ script.js
@@ -0,0 +1,17 @@
+const sliderContainer = document.querySelector('.slider-container');
+const images = sliderContainer.querySelectorAll('img');
+let currentImageIndex = 0;
+showImage(currentImageIndex);
+function showImage(index) {
+  images.forEach(image => image.style.opacity = 0);
+  images[index].style.opacity = 1;
+  images[index].style.transition = "opacity 1s ease-in-out";
+}
+function showNextImage() {
+  currentImageIndex++;
+  if (currentImageIndex >= images.length) {
+    currentImageIndex = 0;
+  }
+  showImage(currentImageIndex);
+}
+setInterval(showNextImage, 5000);
